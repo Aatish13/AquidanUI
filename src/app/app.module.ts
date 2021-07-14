e97bc11e2b6d653e76bf1991e46ef540b65b4ac4
Aatish chaudhari <40951689+Aatish13@users.noreply.github.com>
2021-07-14 12:11:40 +0530

    Top bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { AboutUSComponent } from './about-us/about-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    ContactUsComponent,
+    AboutUSComponent
   ],
   imports: [
     BrowserModule,
