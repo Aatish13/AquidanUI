f9f9990c276d76abf4eaf3689f8b21093da9721c
Aatish chaudhari <40951689+Aatish13@users.noreply.github.com>
2021-12-11 18:38:28 +0530

    Comming Soon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { AboutUSComponent } from './about-us/about-us.component';
 import { HomeComponent } from './home/home.component';
+import { CommingSoonComponent } from './comming-soon/comming-soon.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { HomeComponent } from './home/home.component';
     TopBarComponent,
     ContactUsComponent,
     AboutUSComponent,
-    HomeComponent
+    HomeComponent,
+    CommingSoonComponent
   ],
   imports: [
     BrowserModule,
