dbcfb5c1104f022ec51c86aa6e8d9e8992b5a98e
Aatish chaudhari <40951689+Aatish13@users.noreply.github.com>
2021-12-11 12:54:06 +0530

    angular version upgrade

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { NgModule, ɵSWITCH_COMPILE_COMPONENT__POST_R3__ } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [];
